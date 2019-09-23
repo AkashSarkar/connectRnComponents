292f18694df00a1e66359ce6073d82a699fc903c
Fazle Rabbi Ador <fazle.rabbi@circlefintech.com>
2019-09-23 15:01:13 +0600

    primary button created

--- src/route/index.js
+++ src/route/index.js
@@ -2,12 +2,14 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import App from '../../App';
 import TestRoute from './testRoute/TestRoute';
 import SwipeableWrapperTest from './testRoute/SwipeableWrapperTest';
+import ButtonTest from './testRoute/ButtonTest';
 
 const AppNavigator = createStackNavigator(
   {
     Home: App,
     TestRoute,
     SwipeableWrapperTest,
+    ButtonTest
   },
   {
     initialRouteName: 'Home',
