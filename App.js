e1e326b25627b3c07e58f66ec229bcb74d683870
AkashSarkar <akash.sarkar@circlefintech.com>
2019-09-23 12:28:27 +0600

    Swipeable wrapper

--- App.js
+++ App.js
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -23,92 +15,42 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import SwipeableCard from './src/component/swipeable/SwipeableCard';
+
+const style = StyleSheet.create({
+  boxShadow: {
+    borderRadius: 16,
+    backgroundColor: '#fefefe',
+    shadowColor: '#00000029',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 6,
+    shadowOpacity: 1,
+    elevation: 1,
+    width: '100%',
+    height: 300,
+    marginBottom: 10
+  }
+});
+const BoxShadow = ({ children }) => (
+  <View style={style.boxShadow}>{children}</View>
+);
 
 const App = () => {
   return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+    <View>
+      <SwipeableCard>
+        <BoxShadow>
+          <View style={{justifyContent:"center"}}>
+            <Text>Hello world</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
+        </BoxShadow>
+
+      </SwipeableCard>
+    </View>
+
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
 export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+const assets = {
+  AppIcon: require('./icons/ic_launcher.png'),
+  StarFilled: require('./icons/star_filled.png'),
+  StarUnfilled: require('./icons/star_unfilled.png'),
+  Check: require('./icons/Check.png'),
+  Nfc: require('./icons/Nfc.gif'),
+  Delete: require('./icons/Delete.png'),
+  Forward: require('./icons/Forward.png'),
+  AUX_Logo: require('./icons/AUX_LOGO.png'),
+  Deposit: require('./icons/Deposit.png'),
+  Payment: require('./icons/Payment.png'),
+  Withdrawal: require('./icons/Withdrawal.png'),
+  Transfer: require('./icons/Transfer.png'),
+  LeftArrow: require('./icons/LeftArrow.png')
+};
+
+export default assets;
