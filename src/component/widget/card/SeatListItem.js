7dcd95cd5f9388ecf486a19ce63ab5c8c713bb8a
circlefintech <circlefintech@circlefintechs-Mac-mini.local>
2019-09-23 16:53:02 +0600

    widget/card/SeatListItem complete

--- src/component/widget/card/SeatListItem.js
+++ src/component/widget/card/SeatListItem.js
@@ -4,17 +4,70 @@ import { string, number } from 'prop-types';
 import { TextComponent } from '../../ui'
 import { fonts, colors} from '../../../styles/baseStyle'
 
+const styles = {
+    wrapper: {
+        width: '100%',
+        height: 100,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderRadius: 25,
+        paddingLeft:  10,
+        paddingRight: 10,
+        elevation: 1,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.2,
+                                                             
+    },
+    leftWrapper: {
+
+    },
+    rightWrapper: {
+        alignItems: 'flex-end',
+    }
+}
+
 const SeatListItem = ({
   content, family, size, color
 }) => (
-  <View>
-      <TextComponent 
-        content="hello"
-        color={colors.brandPrimary}
-        family={fonts.light}
-        fontSize={fonts.fs10}
-      />
-  </View>
+    <View style={styles.wrapper}>
+        <View>
+            <TextComponent 
+                content="Desh Travels"
+                color={colors.black1}
+                family={fonts.semiBold}
+                size={fonts.fs16}
+            />
+             <TextComponent 
+                content="110-DHK-CHAP Non AC"
+                color={colors.black1}
+                family={fonts.regular}
+                size={fonts.fs10}
+            />
+             <TextComponent 
+                content="5:45 AM  1:30 PM"
+                color={colors.black1}
+                family={fonts.regular}
+                size={fonts.fs10}
+            />
+        </View>
+        <View style={styles.rightWrapper}>
+            <TextComponent 
+                content="32 Seats Available"
+                color={colors.black1}
+                family={fonts.regular}
+                size={fonts.fs10}
+            />
+            <TextComponent 
+                content="BDT Tk. 450.00"
+                color={colors.black1}
+                family={fonts.semiBold}
+                size={fonts.fs16}
+            />
+        </View>
+    </View>
 );
 
 SeatListItem.propTypes = {
