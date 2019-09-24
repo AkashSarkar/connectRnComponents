e4df2be79921f216a3e14cba919ccf900868d273
rakibhasan <rakib.hasan@circlefintech.com>
2019-09-24 13:31:59 +0600

    tab done

--- src/route/index.js
+++ src/route/index.js
@@ -3,13 +3,17 @@ import App from '../../App';
 import TestRoute from './testRoute/TestRoute';
 import SwipeableWrapperTest from './testRoute/SwipeableWrapperTest';
 import ButtonTest from './testRoute/ButtonTest';
+import TabTest from './testRoute/TabTest';
+import PickerTest from './testRoute/PickerTest';
 
 const AppNavigator = createStackNavigator(
   {
     Home: App,
     TestRoute,
     SwipeableWrapperTest,
-    ButtonTest
+    ButtonTest,
+    TabTest,
+    PickerTest
   },
   {
     initialRouteName: 'Home',
