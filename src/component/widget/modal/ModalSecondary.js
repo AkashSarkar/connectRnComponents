c96543bdea246b2ef23d993b444a19f5af93c410
Fazle Rabbi Ador <fazle.rabbi@circlefintech.com>
2019-09-26 16:59:12 +0600

    modal secondary completed without pressed value callback

--- src/component/widget/modal/ModalSecondary.js
+++ src/component/widget/modal/ModalSecondary.js
@@ -1,37 +1,102 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+  Text, View, Image, TouchableOpacity, ScrollView, FlatList
+} from 'react-native';
 import Modal from 'react-native-modal';
 import TextComponent from '../../ui/typography/TextComponent';
 import { colors, fonts } from '../../../styles/baseStyle';
+import image from '../../../assets';
+
+const renderItem = item => (
+  <View style={{
+    paddingVertical: 10,
+    paddingHorizontal: 30
+  }}
+  >
+    <TouchableOpacity onPress={() => console.warn(item.id)}>
+      <TextComponent
+        content={item.title}
+        size={fonts.fs16}
+        family={fonts.semiBold}
+        color={colors.colorSecondery}
+      />
+    </TouchableOpacity>
+  </View>
+);
 
 const ModalSecondary = ({
-  modalTitle, isVisible, onBackButtonPress
+  modalTitle,
+  isVisible,
+  onBackButtonPress,
+  onClose,
+  items,
+  onSelect
 }) => (
   <Modal
     isVisible={isVisible}
-    backdropColor={colors.black0}
+    backdropColor={colors.black5}
     backdropOpacity={0.5}
     animationIn="zoomInDown"
     animationOut="zoomOutUp"
     animationInTiming={300}
     animationOutTiming={600}
     onBackButtonPress={onBackButtonPress}
+    onBackdropPress={onBackButtonPress}
     backdropTransitionInTiming={300}
     backdropTransitionOutTiming={600}
   >
     <View style={{
+      height: 300,
       backgroundColor: 'white',
-      padding: 22,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 4,
+      padding: 20,
+      borderRadius: 16,
       borderColor: 'black'
     }}
     >
-      <TextComponent
-        content={modalTitle}
-        size={fonts.fs16}
-      />
+      <View style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 20
+      }}
+      >
+        <TextComponent
+          content={modalTitle}
+          size={fonts.fs16}
+          family={fonts.semiBold}
+        />
+        <View style={{
+          position: 'absolute',
+          right: 0
+        }}
+        >
+          <TouchableOpacity onPress={onClose}>
+            <Image
+              style={{
+                height: 35,
+                width: 35
+              }}
+              source={image.Check}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View
+        style={{
+          alignItems: 'center',
+          height: 200,
+          width: '100%'
+        }}
+      >
+        <FlatList
+          data={items}
+          renderItem={({ item }) => (
+            renderItem(item)
+          )}
+          keyExtractor={item => item.id.toString()}
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
     </View>
   </Modal>
 );
--- src/route/testRoute/ModalTest.js
+++ src/route/testRoute/ModalTest.js
@@ -6,10 +6,51 @@ import { ButtonPrimary } from '../../component/ui';
 import { colors, fonts, gradientColors } from '../../styles/baseStyle';
 import { ModalSecondary } from '../../component/widget';
 
+
+const DATA = [
+  {
+    id: '1',
+    title: 'First Item'
+  },
+  {
+    id: '2',
+    title: 'Second Item'
+  },
+  {
+    id: '3',
+    title: 'Third Item'
+  },
+  {
+    id: '4',
+    title: 'First Item'
+  },
+  {
+    id: '5',
+    title: 'Second Item'
+  },
+  {
+    id: '58694a0f-3da1-471f-',
+    title: 'Third Item'
+  },
+  {
+    id: 'bd7acbea-c1b1-46c2-aed53abb28ba',
+    title: 'First Item'
+  },
+  {
+    id: '3ac68af5-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item'
+  },
+  {
+    id: '5864a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item'
+  }
+];
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 10
+    marginTop: 10,
+    justifyContent: 'center'
   },
   viewWrapper: {
     marginBottom: 10
@@ -48,6 +89,9 @@ class ModalTest extends Component {
               modalTitle="Select Operator"
               isVisible={this.state.isModalVisible}
               onBackButtonPress={this.handleClose}
+              onClose={this.handleClose}
+              items={DATA}
+              onSelect={id => console.warn(id)}
             />
           </View>
         </SafeAreaView>
