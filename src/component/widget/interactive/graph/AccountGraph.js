179e20f3d30cdb584b2b91ece2f4e5d9c857f004
rakibhasan <rakib.hasan@circlefintech.com>
2019-10-15 10:23:31 +0600

    in progress, changing branch to upgrade

--- src/component/widget/interactive/graph/AccountGraph.js
+++ src/component/widget/interactive/graph/AccountGraph.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import {
-  Text, View, StyleSheet, SafeAreaView, Dimensions, Animated
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Dimensions,
+  Animated
 } from 'react-native';
 import Svg, {
   Path, Defs, LinearGradient, Stop
@@ -16,7 +20,7 @@ import { BoxShadow, TextComponent } from '../../../ui';
 const height = 100;
 const width = Dimensions.get('window').width - 20;
 const verticalPadding = 10;
-const cursorRadius = 6;
+const cursorRadius = 7;
 const labelWidth = 100;
 
 const d3 = {
@@ -54,7 +58,6 @@ const line = d3.shape
 const properties = path.svgPathProperties(line);
 const lineLength = properties.getTotalLength();
 
-
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1
@@ -77,93 +80,95 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: -45,
     left: 0,
-    // backgroundColor: 'lightgray',
     width: labelWidth
   }
 });
 
 export class AccountGraph extends Component {
-    state = {
-      x: new Animated.Value(0)
-    };
+  state = {
+    x: new Animated.Value(0)
+  };
 
-    cursor = React.createRef();
+  cursor = React.createRef();
 
-    label = React.createRef();
+  label = React.createRef();
 
-    moveCursor(value) {
-      const { x, y } = properties.getPointAtLength(lineLength - value);
-      this.cursor.current.setNativeProps({ top: y - cursorRadius, left: x - cursorRadius });
-      const label = scaleLabel(scaleY.invert(y));
-      this.label.current.setNativeProps({ text: `${label} CHF` });
-    }
+  moveCursor(value) {
+    const { x, y } = properties.getPointAtLength(lineLength - value);
+    this.cursor.current.setNativeProps({
+      top: y - cursorRadius,
+      left: x - cursorRadius
+    });
+    const label = scaleLabel(scaleY.invert(y));
+    console.log(label);
+    this.label.current.setNativeProps({ text: `${label} CHF` });
+  }
 
-    componentDidMount() {
-      this.state.x.addListener(({ value }) => this.moveCursor(value));
-      this.moveCursor(0);
-    }
+  componentDidMount() {
+    this.state.x.addListener(({ value }) => this.moveCursor(value));
+    this.moveCursor(0);
+  }
 
+  render() {
+    const { x } = this.state;
+    const translateX = x.interpolate({
+      inputRange: [0, lineLength],
+      outputRange: [width - labelWidth, 0],
+      extrapolate: 'clamp'
+    });
 
-    render() {
-      const { x } = this.state;
-      const translateX = x.interpolate({
-        inputRange: [0, lineLength],
-        outputRange: [width - labelWidth, 0],
-        extrapolate: 'clamp'
-      });
-
-      return (
-        <SafeAreaView style={styles.rootContainer}>
-          <View style={styles.graphContainer}>
-            <Svg {...{ width, height }}>
-              <Defs>
-                <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="gradient">
-                  <Stop stopColor="#6cf8ff" offset="0%" />
-                  <Stop stopColor="#b5fbff" offset="20%" />
-                  <Stop stopColor="#FEFFFF" offset="100%" />
-                </LinearGradient>
-              </Defs>
-              <Path
-                d={line}
-                fill="transparent"
-                stroke={colors.colorSecondery}
-                strokeWidth={5}
-              />
-              <Path
-                d={`${line} L ${width - 10} ${height} L 0 ${height}`}
-                fill="url(#gradient)"
-              />
-              <View ref={this.cursor} style={styles.cursor} />
-            </Svg>
-            <Animated.View style={[styles.label, { transform: [{ translateX }] }]}>
-              <View ref={this.label}>
-                <BoxShadow>
-                  <TextComponent content="bla" />
-                </BoxShadow>
-              </View>
-            </Animated.View>
-            <Animated.ScrollView
-              style={StyleSheet.absoluteFill}
-              contentContainerStyle={{ width: lineLength * 2 }}
-              showsHorizontalScrollIndicator={false}
-              scrollEventThrottle={16}
-              bounces={false}
-              onScroll={Animated.event(
-                [
-                  {
-                    nativeEvent: {
-                      contentOffset: { x }
-                    }
-                  }
-                ],
-                { useNativeDriver: true },
-              )}
-              horizontal
+    return (
+      <SafeAreaView style={styles.rootContainer}>
+        <View style={styles.graphContainer}>
+          <Svg {...{ width, height }}>
+            <Defs>
+              <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="gradient">
+                <Stop stopColor="#6cf8ff" offset="0%" />
+                <Stop stopColor="#b5fbff" offset="20%" />
+                <Stop stopColor="#FEFFFF" offset="100%" />
+              </LinearGradient>
+            </Defs>
+            <Path
+              d={line}
+              fill="transparent"
+              stroke={colors.colorSecondery}
+              strokeWidth={5}
             />
-          </View>
-        </SafeAreaView>
-      );
-    }
+            <Path
+              d={`${line} L ${width - 10} ${height} L 0 ${height}`}
+              fill="url(#gradient)"
+            />
+            <View ref={this.cursor} style={styles.cursor} />
+          </Svg>
+          <Animated.View style={[styles.label, { transform: [{ translateX }] }]}>
+            <View ref={this.label}>
+              <BoxShadow>
+                <TextComponent content="bla" />
+              </BoxShadow>
+            </View>
+          </Animated.View>
+          <Animated.ScrollView
+            style={StyleSheet.absoluteFill}
+            contentContainerStyle={{ width: lineLength * 2 }}
+            showsHorizontalScrollIndicator={false}
+            scrollEventThrottle={16}
+            bounces={false}
+            onScroll={Animated.event(
+              [
+                {
+                  nativeEvent: {
+                    contentOffset: { x }
+                  }
+                }
+              ],
+              { useNativeDriver: true },
+            )}
+            horizontal
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 export default AccountGraph;
