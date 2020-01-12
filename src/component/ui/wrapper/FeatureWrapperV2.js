e20cb69dd9d6e2d6364e6dff014b84de79c45169
akash <akash.sarkar@circlefintech.com>
2020-01-12 11:00:11 +0600

    feature wrapper,tabslider modified

--- src/component/ui/wrapper/FeatureWrapperV2.js
+++ src/component/ui/wrapper/FeatureWrapperV2.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
-  SafeAreaView, View, Image
+  SafeAreaView, View, Image, TouchableOpacity
 } from 'react-native';
-import { node, string, number } from 'prop-types';
+import { node, string, number, func } from 'prop-types';
 import TextComponent from '../typography/TextComponent';
 import { colors, fonts } from '../../../styles/baseStyle';
 import { p15 } from '../../../styles/commonStyle';
@@ -11,17 +11,6 @@ import assets from '../../../assets';
 const styles = {
   wrapperStyle: {
     height: '100%'
-
-    // paddingVertical: 10,
-    // paddingHorizontal: 20
-    // borderTopLeftRadius: 20,
-    // borderTopRightRadius: 20,
-    // shadowOffset: {
-    //   width: 0,
-    //   height: 3
-    // },
-    // shadowOpacity: 0.5,
-    // elevation: 1
   },
   titleStyle: {
     flexDirection: 'row',
@@ -46,45 +35,64 @@ const FeatureWrapperV2 = (
     title,
     backgroundColor,
     leftIcon,
-    rightIcon
+    rightIcon,
+    titleTextColor,
+    leftPressAction,
+    rightPressAction
   }
 ) => (
   <SafeAreaView style={{ backgroundColor: backgroundColor || colors.black9 }}>
     <View style={styles.wrapperStyle}>
-      {title && title.length > 0 && (
-        <View style={[styles.titleStyle, p15]}>
-          <Image
-            source={leftIcon}
-            style={styles.leftImageStyle}
-          />
+      <View style={[styles.titleStyle, p15]}>
 
+        <TouchableOpacity
+          onPress={leftPressAction}
+          disabled={leftIcon ? false : true}
+        >
+          {leftIcon && (
+            <Image
+              source={leftIcon}
+              style={styles.leftImageStyle}
+            />
+          )}
+        </TouchableOpacity>
+
+        {title && title.length > 0 && (
           <TextComponent
             content={title}
             size={fonts.fs20}
-            color={colors.red2}
+            color={titleTextColor}
             family={fonts.regular}
           />
-
+        )}
+        <TouchableOpacity
+          onPress={rightPressAction}
+        >
           <Image
             source={rightIcon}
             style={styles.crossImageStyle}
           />
+        </TouchableOpacity>
 
-        </View>
-      )}
+      </View>
+
       {children}
     </View>
   </SafeAreaView>
 );
 FeatureWrapperV2.defaultProps = {
-  leftIcon: assets.LeftArrow,
-  rightIcon: assets.Cross
+  // leftIcon: assets.LeftArrow,
+  rightIcon: assets.Cross,
+  titleTextColor: colors.red
 };
 FeatureWrapperV2.propTypes = {
   title: string,
+  titleTextColor: string,
   backgroundColor: string,
   children: node,
   leftIcon: number,
-  rightIcon: number
+  rightIcon: number,
+  leftPressAction: func,
+  rightPressAction: func
 };
 export default FeatureWrapperV2;
--- src/component/widget/tab/TabWrapperV2.js
+++ src/component/widget/tab/TabWrapperV2.js
@@ -10,7 +10,7 @@ import { node, string, bool } from 'prop-types';
 import { BoxShadow, TextComponent } from '../../ui';
 import TabSliderV2 from './TabSliderV2';
 import { colors, fonts } from '../../../styles/baseStyle';
-import { p15 } from '../../../styles/commonStyle';
+import { p15, mb20 } from '../../../styles/commonStyle';
 
 if (
   Platform.OS === 'android'
@@ -32,13 +32,16 @@ const styles = StyleSheet.create({
 const TabWrapperV2 = ({
   tab1Components,
   tab2Components,
+  tabHeader1,
+  tabHeader2,
   tabTitle1,
   tabTitle2,
   tabSubtitle1,
   tabSubtitle2,
   shadowContainer,
   inputFieldBackground,
-  tabBackground
+  tabBackground,
+  headerColor
 }) => {
   const [activeTab, setActiveTab] = useState(1);
   const [shouldTab1Render, setShouldTab1Render] = useState(true);
@@ -73,18 +76,38 @@ const TabWrapperV2 = ({
     <View style={styles.container}>
       {shadowContainer ? (
         <View>
+          <View style={mb20}>
+            {activeTab === 1 ? (
+              <TextComponent
+                content={tabHeader1}
+                size={fonts.fs14}
+                color={headerColor}
+                family={fonts.medium}
+                extraStyle={{ textAlign: 'center' }}
+              />
+            ) : (
+                <TextComponent
+                  content={tabHeader2}
+                  size={fonts.fs14}
+                  color={headerColor}
+                  family={fonts.medium}
+                  extraStyle={{ textAlign: 'center' }}
+                />
+              )}
+          </View>
           <TabSliderV2
             title1={tabTitle1}
             title2={tabTitle2}
             setCurrentTab={setActiveTab}
             tabBackground={tabBackground}
           />
-          <BoxShadow background={inputFieldBackground}>{innerContents()}</BoxShadow>
+          <BoxShadow
+            background={inputFieldBackground}>{innerContents()}</BoxShadow>
         </View>
 
       ) : (
-        <>{innerContents()}</>
-      )}
+          <>{innerContents()}</>
+        )}
     </View>
   );
 };
@@ -92,7 +115,8 @@ const TabWrapperV2 = ({
 TabWrapperV2.defaultProps = {
   shadowContainer: true,
   inputFieldBackground: 'white',
-  tabBackground: 'white'
+  tabBackground: 'white',
+  headerColor: colors.red
 };
 
 TabWrapperV2.propTypes = {
@@ -104,7 +128,8 @@ TabWrapperV2.propTypes = {
   tabSubtitle2: string,
   shadowContainer: bool,
   inputFieldBackground: string,
-  tabBackground: string
+  tabBackground: string,
+  headerColor: string
 };
 
 export default TabWrapperV2;
